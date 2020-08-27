313a2282dbfb5ac152afccba1f6aa5a04e96fe93
Rafael Jimenez <yasieso@gmail.com>
2020-08-27 22:49:03 +0200

    BL dummy

---
+++ GameOfLifeBL/GameLogic.cs
@@ -0,0 +1,28 @@
+﻿using GameOfLifeBL.Models;
+using System;
+
+namespace GameOfLifeBL
+{
+    public static class GameLogic
+    {
+
+        public static Cell[,] Play(Cell[,] board)
+        {
+
+            Cell[,] next = (Cell[,])board.Clone();
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    next[i, j] = new Cell { Alive = true };
+                }
+            }
+
+            return next;
+        }
+
+
+
+    }
+}
---
+++ GameOfLifeBL/Models/Cell.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLifeBL.Models
+{
+    public class Cell
+    {
+        public bool Alive { get; set; }
+
+    }
+}
