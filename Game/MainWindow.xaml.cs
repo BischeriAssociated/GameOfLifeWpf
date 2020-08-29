9942b40f20dc51e20f49d8ebfaf827c37a9d0bbf
Rafael Jimenez <yasieso@gmail.com>
2020-08-29 19:35:50 +0200

    wip

--- Game/MainWindow.xaml.cs
+++ Game/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using GameOfLifeBL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,29 +22,102 @@ namespace Game
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        const int GridFactorSize = 64;
+
         public MainWindow()
         {
             InitializeComponent();
             CreateBoard();
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                //for (int i = 0; i < 10; i++)
+                {
+                    PaintBoard(CraeteRandomBoard());
+                    mainGrid.Children.Clear();
+                }
+            });
         }
 
 
 
         private void CreateBoard()
         {
-            for (int i = 0; i < 64; i++)
+            for (int i = 0; i < GridFactorSize; i++)
             {
                 mainGrid.ColumnDefinitions.Add(new ColumnDefinition { });
                 mainGrid.RowDefinitions.Add(new RowDefinition { });
             }
 
-            AddCell(0, 0);
-            AddCell(1, 0);
+
+
+
+            //AddCell(0, 0);
+            //AddCell(1, 0);
             //mainGrid.Children.Add(txtBlock1);
         }
 
+        private void PaintBoard(Cell[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j].Alive)
+                    {
+                        AddCell(i, j);
+                    }
+                    //else
+                    //{
+                    //    ClearCell(i, j);
+                    //}
+                }
+            }
 
-        private void  AddCell(int x, int y)
+
+        }
+
+
+        #region FOR_TESTS
+        Cell[,] CraeteRandomBoard()
+        {
+            Cell[,] rto = new Cell[GridFactorSize, GridFactorSize];
+            var r = new Random((int)DateTime.Now.Ticks);
+            for (int i = 0; i < rto.GetLength(0); i++)
+            {
+                for (int j = 0; j < rto.GetLength(1); j++)
+                {
+                    rto[i, j] = new Cell { Alive = r.Next(2) == 1 };
+                }
+            }
+
+            return rto;
+        }
+        #endregion FOR_TESTS
+
+
+
+
+
+
+
+
+
+        private void ClearCell(int x, int y)
+        {
+            if (mainGrid.Children.Count != 0)
+            {
+                int index = (x == y && x == 0) ? 0 : (x * GridFactorSize + y) - 1;
+                var reo = mainGrid.Children[index];
+                if (reo != null)
+                {
+                    mainGrid.Children.Remove(reo);
+                }
+            }
+        }
+
+        private void AddCell(int x, int y)
         {
             Ellipse circle = new Ellipse
             {
